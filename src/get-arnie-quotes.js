a12e08351bf3e7e9c58e8c1fbd4b6a647b06ee60
Bhaskara Duvvuri <bhaskar.duvvuri@gmail.com>
2021-05-23 00:17:13 +1000

    Implement simple getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,36 @@
 const { httpGet } = require('./mock-http-interface');
 
+const arnieQuoteKey = 'Arnie Quote';
+const failureKey = 'FAILURE';
+
+const handleJsonParse = (response) => {
+  try {
+    return response && response.body && JSON.parse(response.body);
+  } catch(error) {
+    return null;
+  }
+};
+
+const handleResponse = (response, body) => {
+  if(response.status && response.status === 200) {
+    return {[arnieQuoteKey]: body.message};
+  }
+  return {[failureKey]: body.message};
+};
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const allHttpGets = urls.map(url => {
+    return httpGet(url);
+  });
+  const responses = await Promise.all(allHttpGets);
+  const results = responses.map(response => {
+    const body = handleJsonParse(response);
+    if(body && body.message) {
+      return handleResponse(response, body);
+    }
+    return null;
+  });
+  return results;
 };
 
 module.exports = {
